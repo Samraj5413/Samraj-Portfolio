504eade32e705acc095ce9c2c87275d6c5d36548
Samraj5413 <samraj6083@gmail.com>
2025-03-30 17:10:24 +0530

    modified project section

--- src/App.tsx
+++ src/App.tsx
@@ -190,6 +190,10 @@ function App() {
                 title: 'Calculator App',
                 description: 'A simple and responsive calculator built using React.js and TailwindCSS. It supports basic arithmetic operations, percentage calculations, and features a modern dark-themed UI.',
                 link: 'https://github.com/Samraj5413/Calculator-App'
+              },{
+                title: 'E-Commerce Website',
+                description: 'A modern and responsive e-commerce web application built using React.js, TypeScript, and TailwindCSS. This project provides a smooth shopping experience with an interactive UI and real-time cart functionality.',
+                link: 'https://github.com/Samraj5413/E-Commerce-Project'
               },
             ].map((project, index) => (
               <div 
